6dbd31ef716db8af3bb95506d272f6c1318258e8
Denys M <dector9@gmail.com>
2019-12-14 12:59:02 +0400

    Add QrCode implementation from nayuki
    https://github.com/nayuki/QR-Code-generator/tree/master/java

--- src/main/kotlin/io/github/dector/quark/main.kt
+++ src/main/kotlin/io/github/dector/quark/main.kt
@@ -1,5 +1,14 @@
 package io.github.dector.project
 
+import io.nayuki.qrcodegen.QrCode
+import javax.imageio.ImageIO
+
 fun main() {
-    println("It works!")
+    val qrCode = QrCode.encodeText("https://kotlinlang.org", QrCode.Ecc.LOW)
+
+    ImageIO.write(
+        qrCode.toImage(16, 4),
+        "png",
+        File("out/qr_code.png")
+    )
 }
